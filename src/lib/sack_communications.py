51516c828a180867630df50872e8e7f636f3b314
vic02505 <vzacarias@fi.uba.ar>
2024-09-30 13:23:19 -0300

    codigo basico para el receiver del server, refactor del sack communications para poder mandar sack content, me pego una ducharda y vuelvo al ring, wesaaaa

--- src/lib/sack_communications.py
+++ src/lib/sack_communications.py
@@ -32,10 +32,16 @@ SACK_DATAGRAM_SIZE = 1459
 '''
 
 class SackDatagramDeserialized:
+
     def __init__(self, bytes_flow):
+        self.first_sack = None
+        self.second_sack = None
+        self.third_sack = None
+        self.fourth_sack = None
+
         self.datagram_type = struct.unpack('<B', bytes_flow[0:1])[0] # 1 byte
         self.number_of_sacks = struct.unpack('<B', bytes_flow[1:2])[0] # 1 byte
-        self.sacks_content = bytes_flow[2:34].decode().rstrip('\x00')  # 32 bytes "2-3,5-6"
+        self.get_sacks(bytes_flow[2:34]) #32 bytes
         self.file_name = bytes_flow[34:134].decode().rstrip('\x00') # 100
         self.datagram_number = struct.unpack('<I', bytes_flow[134:138])[0] # 4 bytes
         self.total_datagrams = struct.unpack('<I', bytes_flow[138:142])[0] # 4 bytes
@@ -43,6 +49,26 @@ class SackDatagramDeserialized:
         self.content = bytes_flow[115:1430] # 1315 bytes
         self.content = self.content[:self.content_size]
 
+    def get_sacks(self, sack_bytes_flow):
+        if self.number_of_sacks >= AmountOfSacks.ONE_SACK.value:
+            first = struct.unpack('<I', sack_bytes_flow[0:4])[0]
+            second = struct.unpack('<I', sack_bytes_flow[4:8])[0]
+            self.first_sack = (first, second)
+
+        if self.number_of_sacks >= AmountOfSacks.TWO_SACKS.value:
+            first = struct.unpack('<I', sack_bytes_flow[8:12])[0]
+            second = struct.unpack('<I', sack_bytes_flow[12:16])[0]
+            self.second_sack = (first, second)
+
+        if self.number_of_sacks >= AmountOfSacks.THREE_SACKS.value:
+            first = struct.unpack('<I', sack_bytes_flow[16:20])[0]
+            second = struct.unpack('<I', sack_bytes_flow[20:24])[0]
+            self.third_sack = (first, second)
+
+        if self.number_of_sacks >= AmountOfSacks.FOUR_SACKS.value:
+            first = struct.unpack('<I', sack_bytes_flow[24:28])[0]
+            second = struct.unpack('<I', sack_bytes_flow[28:32])[0]
+            self.fourth_sack = (first, second)
 
 class SackDatagram:
     def __init__(self, datagram_type, number_of_sacks, sacks_content, file_name, datagram_number, total_datagrams,
@@ -57,9 +83,12 @@ class SackDatagram:
         self.content =  content.ljust(1315, b'0')  #  1315 bytes
 
     def get_datagram_bytes(self):
-        format = '<BB32s100sIIH1315s'
-        return struct.pack(format, self.datagram_type, self.number_of_sacks, self.sacks_content, self.file_name,
-                           self.datagram_number, self.total_datagrams, self.content_size, self.content)
+        format = '<BB8s8s8s8s100sIIH1315s'
+        return struct.pack(format, self.datagram_type, self.number_of_sacks,
+                           self.sacks_content[0][0], self.sacks_content[0][1],self.sacks_content[1][0],
+            self.sacks_content[1][1], self.sacks_content[2][0], self.sacks_content[2][1], self.sacks_content[3][0],
+            self.sacks_content[3][1], self.file_name,self.datagram_number, self.total_datagrams, self.content_size,
+                           self.content)
 
     @classmethod
     def create_content(cls, datagram_number, total_datagrams, content_size, content):
@@ -68,8 +97,11 @@ class SackDatagram:
                    total_datagrams=total_datagrams, content_size=content_size, content=content)
 
     @classmethod
-    def create_ack(cls, number_of_sack, sacks_content, ack_number):
-        return cls(datagram_type=TypeOfSackDatagram.ACK.value, number_of_sacks=number_of_sack,
+    def create_ack(cls, number_of_sacks, sacks_content, ack_number):
+
+        #co
+
+        return cls(datagram_type=TypeOfSackDatagram.ACK.value, number_of_sacks=number_of_sacks,
                    sacks_content=sacks_content, file_name="", datagram_number=ack_number, total_datagrams=0,
                    content_size=0, content=b"")
 
--- src/lib/selective_ack_vic.py
+++ src/lib/selective_ack_vic.py
@@ -29,70 +29,25 @@ class SelectiveAcK:
         return cls(origin_address=origin_address, destination_address=destination_address, is_server=True,
                    socket=communication_socket, messages_queue=None, messages_vector=None, congestion_window=None)
 
-    def send_ack(self):
-        pass
+    def start_server(self):
+        sack_datagram = self.messages_queue.get()
+        client_message = SackDatagramDeserialized(sack_datagram)
 
-    def receiving_operation_for_client(self):
+        if client_message.datagram_type == TypeOfSackDatagram.UPLOAD.value:
+            file_name = client_message.file_name
+            total_datagrams = client_message.total_datagrams
+            datagram_number = client_message.datagram_number
+            self.send_ack()
+            self.receiving_operation_for_server()
+        elif client_message.datagram_type == TypeOfSackDatagram.DOWNLOAD.value:
+            file_name = client_message.file_name
+            file_size = files_management.get_file_size(file_name)
+            total_datagrams = files_management.get_count_of_datagrams(file_name)
 
-        pass
-    def sending_operation_for_client(self, file_name):
-        ## Completar
-        try:
-            file_contents = files_management.get_file_content(file_name)
-        except:
-            raise "Archivo no encontrado"
-
-        datagrams = files_management.get_datagrams_for_sack(file_contents)
-
-        sent_datagrams = [None]*self.congestion_window_size # (Datagrama, Hora de envio)
-        recognized_file_fragments = 0
-        datagrams_in_congestion_window = 0
-
-        self.socket.settimeout(0,1)
-        self.socket.setblocking(False)
-        # Envio todos los datagramas de una
-
-        while recognized_file_fragments < len(datagrams):
-
-            while  datagrams_in_congestion_window < self.congestion_window_size:
-
-                # TODO: en la primer iteracion se envian los 5 datagrams de una, pero desp quizas hay que enviar
-                #       datagramas especificos. Tener en cuenta para el sendto
-                # TODO: Actualizar hora de envio de datagrama enviado
-
-                datagram = datagrams[datagrams_in_congestion_window]
-                self.socket.sendto(datagram.get_datagram_bytes(), self.destination_address)
-
-                sent_datagrams[datagrams_in_congestion_window] = (datagram, time.time())
-                datagrams_in_congestion_window += 1
-
-
-            ack, client_address = self.socket.recvfrom(SACK_DATAGRAM_SIZE)
-            ack_deserialized = SackDatagramDeserialized(ack)
-            #  Si no llego alguno modificar la variable datagrams_in_congestion_window restando
-
-            ack_number = ack_deserialized.datagram_number
-
-            sacks_content = ack_deserialized.sacks_content.split(',')
-            for sack in sacks_content:
-                initial_position, final_position = sack.split('-')
-                initial_position = int(initial_position)
-                final_position = int(final_position)
-                # TODO: Manejar estas posiciones y el ack
-
-            # datagrams_to_resend = []
-            # # TODO: Manejo de los timestamps
-            # for datagram_sended in sent_datagrams:
-            #
-            #     ts_datagram = sent_datagrams[datagram_sended].1
-            #     if ts_datagram:
-            #         ts = time.time()
-            #         if (ts - ts_datagram) > 0.5:
-            #             # TODO: Tengo que volver a enviarlo porque hubo perdida de paquetes
-            #             datagrams_to_resend.append(datagram_sended)
-            #
-            #             pass
-        pass
+            self.send_ack()
+            self.sending_operation_for_server()
+        else:
+            raise Exception("El primer mensaje no es un header")
 
     def start_client(self, file_name, datagram_type):
         self.socket.settimeout(TIMEOUT_CLIENT)
@@ -119,6 +74,84 @@ class SelectiveAcK:
         else:
            raise Exception(f"[Servidor - Hilo #{self.destination_address}] El primer mensaje no es un header")
 
+    def send_ack(self):
+        pass
+
+    def receiving_operation_for_client(self):
+        pass
+
+    @staticmethod
+    def get_congestion_window_first_free_position(sent_datagrams):
+        for i in range(len(sent_datagrams)):
+            if sent_datagrams[i] is None:
+                return i
+
+    @staticmethod
+    def update_congestion_window():
+       pass
+
+
+    def sending_operation_for_client(self, file_name):
+        ## Completar
+        try:
+            file_contents = files_management.get_file_content(file_name)
+        except:
+            raise "Archivo no encontrado"
+
+        datagrams = files_management.get_datagrams_for_sack(file_contents)
+
+        congestion_window = [None] * self.congestion_window_size # (Datagrama, Hora de envio)
+        recognized_file_fragments = 0
+        datagrams_in_congestion_window = 0
+        next_datagram_to_send = 0
+
+        self.socket.settimeout(0,1) # setearlo en start client
+        self.socket.setblocking(False)
+
+        while recognized_file_fragments < len(datagrams):
+
+            while  datagrams_in_congestion_window < self.congestion_window_size:
+
+                # TODO: en la primer iteracion se envian los 5 datagrams de una, pero desp quizas hay que enviar
+                #       datagramas especificos. Tener en cuenta para el sendto
+                # TODO: Actualizar hora de envio de datagrama enviado
+
+                datagram = datagrams[next_datagram_to_send]
+                self.socket.sendto(datagram.get_datagram_bytes(), self.destination_address)
+                free_position = self.get_congestion_window_first_free_position(congestion_window)
+                congestion_window[free_position] = (datagram, time.time())
+
+                datagrams_in_congestion_window += 1
+                next_datagram_to_send += 1
+
+            # puede pasar que no se reciba nada, deberia estar en try exception?
+            ack, client_address = None, None
+            ack, client_address = self.socket.recv(SACK_DATAGRAM_SIZE)
+
+            if ack is None:
+                continue
+
+            ack_deserialized = SackDatagramDeserialized(ack)
+            datagrams_in_congestion_window -= 1
+            ack_number = ack_deserialized.datagram_number
+
+            
+            # datagrams_to_resend = []
+            # # TODO: Manejo de los timestamps
+            # for datagram_sended in congestion_window:
+            #
+            #     ts_datagram = sent_datagrams[datagram_sended].1
+            #     if ts_datagram:
+            #         ts = time.time()
+            #         if (ts - ts_datagram) > 0.5:
+            #             # TODO: Tengo que volver a enviarlo porque hubo perdida de paquetes
+            #             datagrams_to_resend.append(datagram_sended)
+            #
+            #             pass
+            recognized_file_fragments = calculate_recog_file_datagrams(ack_number, sacks)
+        pass
+
+
     # Wait ack: espera que llegue un ack, si no llega, reenvia el datagrama
     def wait_ack_client(self, datagram):
         # Mientras no recibo el ack, mando de vuelta el datagram
@@ -137,37 +170,28 @@ class SelectiveAcK:
                 print(f"[Cliente - {self.origin_address}] Datagrama enviado nuevamente")
 
 
-    def find_hole(self, received_data, begin, end):
+    @staticmethod
+    def get_second_sack_position(begin, received_data):
+        for i in range(begin + 1, len(received_data)):
 
-        actual_position = begin
+            if received_data[i] - received_data[i - 1] > 1:
+                return received_data[i - 1] + 1
 
-        hole_founded = False
-        hole_begin = -1
-        hole_end = -1
+    def build_sacks_from_received_data(self, received_data, amount_of_datagrams):
+        list_of_sacks = []
 
-        while begin < end:
+        for i in range(1, len(received_data)):
 
-            if (not hole_founded) and (received_data[actual_position] == -1):
-                hole_founded = True
-                hole_begin = actual_position
-            elif (hole_founded)
+            if (received_data[i] - received_data[i - 1]) > 1:
+                first_sack_possition = received_data[i]
+                second_sack_position = self.get_second_sack_position(i, received_data)
 
-        return hole_begin, hole_end
+                if second_sack_position is None:
+                    second_sack_position = amount_of_datagrams
 
+                list_of_sacks.append((first_sack_possition, second_sack_position))
 
-    def get_sack(self, received_data):
-
-        search_is_finished = False
-
-
-        while not search_is_finished:
-
-            for i in range(received_data):
-
-                if received_data[i] == -1:
-
-
-
+        return list_of_sacks
 
     def receiving_operation_for_server(self, amount_of_datagrams):
 
@@ -183,31 +207,13 @@ class SelectiveAcK:
             else:
                 print("Me mandaste que ya recibi capo")
 
-        self.get_sack()
+            sacks_list = self.build_sacks_from_received_data(received_data, amount_of_datagrams)
+            ack_datagram = SackDatagram.create_ack(len(sacks_list), sacks_list, 1)
+            ack_datagram_bytes = ack_datagram.get_datagram_bytes()
 
-
-        pass
+            self.socket.sendto(ack_datagram_bytes, client_address)
 
     def sending_operation_for_server(self):
         pass
 
-    def start_server(self):
-        sack_datagram = self.messages_queue.get()
-        client_message = SackDatagramDeserialized(sack_datagram)
-
-        if client_message.datagram_type == TypeOfSackDatagram.UPLOAD.value:
-            file_name = client_message.file_name
-            total_datagrams = client_message.total_datagrams
-            datagram_number = client_message.datagram_number
-            self.send_ack()
-            self.receiving_operation_for_server()
-        elif client_message.datagram_type == TypeOfSackDatagram.DOWNLOAD.value:
-            file_name = client_message.file_name
-            file_size = files_management.get_file_size(file_name)
-            total_datagrams = files_management.get_count_of_datagrams(file_name)
-
-            self.send_ack()
-            self.sending_operation_for_server()
-        else:
-            raise Exception("El primer mensaje no es un header")
 
